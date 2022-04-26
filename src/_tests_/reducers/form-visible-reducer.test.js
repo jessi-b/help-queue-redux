03ff4b2df3c170018a734f0500c4998185ccfa6a
jb <jessb.epicodus@gmail.com>
2022-04-26 13:11:00 -0700

    pass toggle form

--- src/_tests_/reducers/form-visible-reducer.test.js
+++ src/_tests_/reducers/form-visible-reducer.test.js
@@ -1,8 +1,12 @@
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 
 describe("formVisibleReducer", () => {
-
-  test('Should return default state if no action type is recognized', (npm run test) => {
+//initial state
+  test('Should return default state if no action type is recognized', () => {
     expect(formVisibleReducer(false, { type: null })).toEqual(false);
+  });
+//toggle form
+  test('Should toggle form visibility state to true', () => {
+    expect(formVisibleReducer(false, { type: 'TOGGLE_FORM' })).toEqual(true);
   });
 });
